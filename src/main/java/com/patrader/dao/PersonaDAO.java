7137010856f0a3873e6317914284b2e6709b1026
alex <alejandrokmz@gmail.com>
2019-05-16 17:14:16 -0500

    funcion inser
    funcion inser

--- src/main/java/com/patrader/dao/PersonaDAO.java
+++ src/main/java/com/patrader/dao/PersonaDAO.java
@@ -3,25 +3,42 @@ package com.patrader.dao;
 import com.patrader.dto.Persona;
 import com.patrader.ConexionMySQL;
 
+import javax.ws.rs.core.Response;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PersonaDAO {
 
+    private static PersonaDAO instance;
 
-    public String getNombreHic(int idBulto) {
-        StringBuilder qry = new StringBuilder();
-        qry.append(" SELECT identificador from sgdbulto ");
-        qry.append(" WHERE id = " + idBulto + ";  ");
+    public static PersonaDAO getInstance() {
+        if (instance == null)
+            instance = new PersonaDAO();
+        return instance;
+    }
+    public List<Persona> seleccionarPersonas() {
+        String qry = " SELECT id,nombre,ap_pat,ap_mat,fecha_creacion FROM personas";
         ConexionMySQL conexion = ConexionMySQL.getInstance();
         ResultSet rs;
-        try {
-            conexion.setQry(qry.toString());
-            rs = conexion.getSelect();
+        Persona pers;
 
-            if (rs.next()) {
-                return rs.getString(1);
+        try {
+            conexion.setQry(qry);
+            rs = conexion.getSelect();
+            List<Persona> lista = new ArrayList<Persona>();
+
+            while (rs.next()) {
+                pers = new Persona();
+                pers.setId(rs.getInt("id"));
+                pers.setNombre(rs.getString("nombre"));
+                pers.setApellidoPaterno(rs.getString("ap_pat"));
+                pers.setApellidoMaterno(rs.getString("ap_mat"));
+                lista.add(pers);
+
             }
+            return lista;
         } catch (Exception ex) {
             ex.printStackTrace();
         } finally {
@@ -31,27 +48,31 @@ public class PersonaDAO {
     }
 
 
-    public boolean insertarHic(Persona persona) {
-        String qry = " INSERT INTO sgdBulto (idDesecho, revprocedimiento, anio, unidad, consecutivo, porcentaje, identificador, pendienteClas, observacionClasificacion, fechaCreacion, revisoClas, realizoClas) ";
-        qry += "       VALUES (?, ?, ?, ?, ?, ?, TRIM(?), ?, ?, NOW(), ?, ? ); ";
+
+
+    public Response insert(Persona persona) {
+
+        String qry = "INSERT INTO personas SET NOMBRE=?, AP_PAT=?, AP_MAT=?, FECHA_CREACION=NOW();";
         ConexionMySQL conexion = ConexionMySQL.getInstance();
-        PreparedStatement preparedStmt = null;
+
         try {
             conexion.connect();
-            // Insertar en tabla sgdbultoLote
-             preparedStmt = conexion.getConnection().prepareStatement(qry);
-            //preparedStmt.setInt(1, hic.getDesecho().getClave());
+            PreparedStatement preparedStmt = conexion.getConnection().prepareStatement(qry);
+            preparedStmt.setString(1, persona.getNombre());
+            preparedStmt.setString(2, persona.getApellidoPaterno());
+            preparedStmt.setString(3, persona.getApellidoMaterno());
 
+            int result = preparedStmt.executeUpdate();
 
-            // execute the preparedstatement
-           return preparedStmt.execute();
+            if (result > 0)
+                return Response.status(201).entity(persona).build();
 
-        } catch (Exception e) {
-            BitacoraErrorDAO.getInstance().insertBitacoraError("SQL",e.getMessage(),preparedStmt.toString());
-            e.printStackTrace();
+        } catch (Exception ex) {
+            BitacoraErrorDAO.getInstance().insertBitacoraError("SQL", ex.getMessage(), qry);
+            ex.printStackTrace();
         } finally {
-            ConexionMySQL.getInstance().close();
+            conexion.close();
         }
-        return false;
+        return Response.status(400).entity("Syntaxis invalida").build();
     }
 }
--- src/main/java/com/patrader/rest/PersonasREST.java
+++ src/main/java/com/patrader/rest/PersonasREST.java
@@ -1,28 +1,31 @@
 package com.patrader.rest;
 
+import com.patrader.dao.PersonaDAO;
 import com.patrader.dto.Persona;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import java.util.ArrayList;
 import java.util.List;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
 
 @Path("personas")
 public class PersonasREST {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public List<Persona> select(){
-        List<Persona> lista = new ArrayList<Persona>();
-        Persona p = new Persona();
-        p.setId(1);
-        p.setNombre("ALEJANDRO");
-        p.setApellidoPaterno("CARMONA");
-        p.setApellidoMaterno("MENDOZA");
+    public List<Persona> get(){
+        return PersonaDAO.getInstance().seleccionarPersonas();
+    }
 
-        lista.add(p);
-        return lista;
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response insert(Persona persona){
+        return PersonaDAO.getInstance().insert(persona);
     }
 }
